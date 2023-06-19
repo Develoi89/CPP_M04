7c78bde8db85e6ff1d5a30c5615c3c81eb854ff6
Eloi Alonso Quiñonero <97552454+Develoi89@users.noreply.github.com>
2023-06-19 18:32:12 +0200

    ice class corrected

---
+++ ex03/Ice.cpp
@@ -0,0 +1,23 @@
+#include "Ice.hpp"
+
+Ice::Ice(): _type("ice")
+{
+}
+Ice::Ice(Ice const & c)
+{
+    _type = c._type;
+}
+Ice & Ice::operator = (Ice const & c)
+{
+    _type = c._type;
+    return *this;
+}
+Ice::~Ice()
+{
+}
+
+AMateria* Ice::clone() const
+{
+    AMateria * c = new Ice;
+    return c;
+}
