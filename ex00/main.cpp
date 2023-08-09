2398b933fce8b8111f759f7ba060e6b95031044e
Eloi Alonso Quiñonero <97552454+Develoi89@users.noreply.github.com>
2023-08-09 15:17:02 +0200

    ex00 slved

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,17 +6,29 @@
 
 int main()
 {
-const Animal* meta = new Animal();
-const Animal* j = new Dog();
-const Animal* i = new Cat();
-std::cout << j->getType()<< " " << std::endl;
-std::cout << i->getType() << " " << std::endl;
-i->makeSound(); //will output the cat sound!
-j->makeSound();
-meta->makeSound();
-delete meta;
-delete j;
-delete i;
+    {
+    const Animal* meta = new Animal();
+    const Animal* j = new Dog();
+    const Animal* i = new Cat();
+    std::cout << j->getType()<< " " << std::endl;
+    std::cout << i->getType() << " " << std::endl;
+    i->makeSound(); //will output the cat sound!
+    j->makeSound();
+    meta->makeSound();
+    delete meta;
+    delete j;
+    delete i;
+    }
+std::cout << std::endl;
+std::cout << std::endl;
+    {
 
-return 0;
+    const WrongAnimal* i = new WrongCat();
+    std::cout << i->getType() << " " << std::endl;
+    i->makeSound(); //will output the cat sound!
+    delete i;
+
+    return 0;
+
+    }
 }
